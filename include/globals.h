f05025cea624242578efae219ee8619ecbb194a4
NESEGEMAA <nesegemaa@gmail.com>
2025-05-10 09:26:49 +0300

    Added PC and SREG as global variables in globals.h

--- include/globals.h
+++ include/globals.h
@@ -9,4 +9,8 @@
 typedef uint8_t  data_word_t;     // 8-bit data word
 typedef uint16_t instruction_t;    // 16-bit instruction word
 
+// Global variable declarations
+extern instruction_t PC;          // Program Counter
+extern data_word_t SREG;         // Status Register
+
 #endif // GLOBALS_H
